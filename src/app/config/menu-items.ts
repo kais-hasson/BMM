73c71d851c09099dc086550280f39ce2a2b5f7ed
kais_dev <kaishasson9@gmail.com>
2025-10-24 16:59:42 +0300

    fix navbar and responsive

--- src/app/config/menu-items.ts
+++ src/app/config/menu-items.ts
@@ -2,6 +2,7 @@ export class NavigationItem {
   icon: any;
   title: string='';
   link: string='';
+  id:number=0;
 }
 export const defaultMenuItems: NavigationItem [] =
   [
@@ -9,25 +10,30 @@ export const defaultMenuItems: NavigationItem [] =
       title: 'Home',
       icon: '',
       link: '/',
+      id:1,
     },
     {
       title: 'Color Models',
       icon: '',
       link: '/color-models',
+      id:2,
     },
     {
       title: 'Digital Images',
       icon: '',
       link: '/digital-images',
+      id:3,
     },
     {
       title: 'AI Gallery',
       icon: '',
       link: '/ai-gallery-images',
+      id:4,
     },
     {
       title: 'About',
       icon: '',
       link: '/about',
+      id:5,
     },
   ] as NavigationItem[];
--- src/app/nav-bar/nav-bar.ts
+++ src/app/nav-bar/nav-bar.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { NgOptimizedImage} from '@angular/common';
 import {defaultMenuItems} from '../config/menu-items';
+import {Router, RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, RouterLink, RouterLinkActive],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.scss'
 })
 export class NavBar {
  navigationItems= defaultMenuItems
+  constructor(public router: Router) {}
+  isActive(url: string): boolean {
+    // second param true => exact match
+    return this.router.isActive(url, true);
+  }
 }
